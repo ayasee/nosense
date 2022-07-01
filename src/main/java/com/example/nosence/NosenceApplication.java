77abf798f1e92223deef34276ff1f950da31a3c8
lqm <767892459@qq.com>
2022-07-01 15:21:10 +0800

    1

---
+++ src/main/java/com/example/nosence/NosenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nosence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nosence/NosenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nosence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
