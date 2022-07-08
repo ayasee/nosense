22c765ca0e0d1beb2368a5827515bebeb1ba0b89
ayase <767892459@qq.com>
2022-07-08 14:00:14 +0800

    nono

--- src/main/java/com/example/nosence/NosenceApplication.java
+++ src/main/java/com/example/nosence/NosenceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NosenceApplication {
-
+    public void hh() {}
     public static void main(String[] args) {
         SpringApplication.run(NosenceApplication.class, args);
     }
